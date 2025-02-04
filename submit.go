4a4dcbb80edc53d730eb86390518815c8124f4fb
1Conan <me@1conan.com>
2025-02-05 06:53:43 +0800

    Include room features description

--- submit.go
+++ submit.go
@@ -1014,6 +1014,7 @@ func (s *submitServer) buildReportBody(ctx context.Context, p parsedPayload, lis
 	var roomDescription string
 	var attachmentDescription string
 	var latestReadReceipt string
+	var roomFeatures string
 	for k := range p.Data {
 		switch k {
 		case "event_id", "room_id", "event_timestamp":
@@ -1026,6 +1027,8 @@ func (s *submitServer) buildReportBody(ctx context.Context, p parsedPayload, lis
 			attachmentDescription = p.Data[k]
 		case "latest_read_receipt":
 			latestReadReceipt = p.Data[k]
+		case "room_features":
+			roomFeatures = p.Data[k]
 		default:
 			dataKeys = append(dataKeys, k)
 		}
@@ -1039,6 +1042,9 @@ func (s *submitServer) buildReportBody(ctx context.Context, p parsedPayload, lis
 	}
 	if roomDescription != "" {
 		_, _ = fmt.Fprintf(&bodyBuf, "### Room description:\n\n```json\n%s\n```\n", roomDescription)
+	}
+	if roomFeatures != "" {
+		_, _ = fmt.Fprintf(&bodyBuf, "### Room features:\n\n```json\n%s\n```\n", roomFeatures)
 	}
 	if attachmentDescription != "" {
 		_, _ = fmt.Fprintf(&bodyBuf, "### Attachment description:\n\n```json\n%s\n```\n", attachmentDescription)
